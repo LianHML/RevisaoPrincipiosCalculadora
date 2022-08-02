1099917f3fab322ce2a7f7dd5beb27f3d4f1f546
Lian <lianlinsmeyer@gmail.com>
2022-08-02 09:57:38 -0300

    Aplicação do Menu com os métodos de operação

--- Program.cs
+++ Program.cs
@@ -21,12 +21,23 @@ namespace RevisaoPrincipiosCalculadora
             Console.WriteLine("4 - Multiplicação");
             Console.WriteLine("");
             Console.WriteLine("Selecione uma opção: ");
+
             short operacaoSelecionada = short.Parse(Console.ReadLine());
+
+            switch(operacaoSelecionada)
+            {
+                case 1: Soma(); break;
+                case 2: Subtracao(); break;
+                case 3: Divisao(); break;
+                case 4: Multiplicacao(); break;
+                default: Menu(); break;
+            }
         }
 
         static void Soma()
         {
             Console.Clear();
+            Console.WriteLine("Soma");
             Console.WriteLine("Primeiro Valor: ");
             float primeiroValor = float.Parse(Console.ReadLine());
 
@@ -37,11 +48,15 @@ namespace RevisaoPrincipiosCalculadora
 
             float resultado = primeiroValor + segundoValor;
             Console.WriteLine($"O Resultado da soma é: {resultado}");
+
+            Console.ReadKey();
+            Menu();
         }
         
         static void Subtracao()
         {
             Console.Clear();
+            Console.WriteLine("Subtração");
             Console.WriteLine("Primeiro Valor: ");
             float primeiroValor = float.Parse(Console.ReadLine());
 
@@ -52,11 +67,15 @@ namespace RevisaoPrincipiosCalculadora
 
             float resultado = primeiroValor - segundoValor;
             Console.WriteLine($"O Resultado da subtração é: {resultado}");
+
+            Console.ReadKey();
+            Menu();
         }
 
         static void Divisao()
         {
             Console.Clear();
+            Console.WriteLine("Divisão");
             Console.WriteLine("Primeiro Valor: ");
             float primeiroValor = float.Parse(Console.ReadLine());
 
@@ -67,11 +86,15 @@ namespace RevisaoPrincipiosCalculadora
 
             float resultado = primeiroValor / segundoValor;
             Console.WriteLine($"O resultado da divisão é: {resultado}");
+
+            Console.ReadKey();
+            Menu();
         }
 
         static void Multiplicacao()
         {
             Console.Clear();
+            Console.WriteLine("Multiplicação");
             Console.WriteLine("Primeiro Valor: ");
             float primeiroValor = float.Parse(Console.ReadLine());
 
@@ -82,6 +105,11 @@ namespace RevisaoPrincipiosCalculadora
 
             float resultado = primeiroValor * segundoValor;
             Console.WriteLine($"O resultado da multiplicação é: {resultado}");
+
+            Console.ReadKey();
+            Menu();
         }
+
     }
+
 }
