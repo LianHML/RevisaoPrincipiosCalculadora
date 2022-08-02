37960cd5fc462ca28e742b63321439e29d5ee080
Lian <lianlinsmeyer@gmail.com>
2022-08-02 09:35:55 -0300

    Criação do método "Subtracao"

--- Program.cs
+++ Program.cs
@@ -23,5 +23,20 @@ namespace RevisaoPrincipiosCalculadora
             float resultado = primeiroValor + segundoValor;
             Console.WriteLine($"O Resultado da soma é: {resultado}");
         }
+        
+        static void Subtracao()
+        {
+            Console.Clear();
+            Console.WriteLine("Primeiro Valor: ");
+            float primeiroValor = float.Parse(Console.ReadLine());
+
+            Console.WriteLine("Segundo Valor: ");
+            float segundoValor = float.Parse(Console.ReadLine());
+
+            Console.WriteLine("");
+
+            float resultado = primeiroValor - segundoValor;
+            Console.WriteLine($"O Resultado da subtração é: {resultado}");
+        }
     }
 }
