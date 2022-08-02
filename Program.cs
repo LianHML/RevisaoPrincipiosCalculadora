2157419d7e603a88ff87ddc6741d101863a3390f
Lian <lianlinsmeyer@gmail.com>
2022-08-02 09:42:25 -0300

    Criação do método "Divisão"

--- Program.cs
+++ Program.cs
@@ -38,5 +38,20 @@ namespace RevisaoPrincipiosCalculadora
             float resultado = primeiroValor - segundoValor;
             Console.WriteLine($"O Resultado da subtração é: {resultado}");
         }
+
+        static void Divisao()
+        {
+            Console.Clear();
+            Console.WriteLine("Primeiro Valor: ");
+            float primeiroValor = float.Parse(Console.ReadLine());
+
+            Console.WriteLine("Segundo Valor: ");
+            float segundoValor = float.Parse(Console.ReadLine());
+
+            Console.WriteLine("");
+
+            float resultado = primeiroValor / segundoValor;
+            Console.WriteLine($"O resultado da divisão é: {resultado}");
+        }
     }
 }
